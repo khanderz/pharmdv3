3389519957ab225170779aa8bd8d4a2b7f9d1e06
Khanh <82117702+khanderz@users.noreply.github.com>
2024-03-11 19:49:37 -0500

    fix errors

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,7 +1,9 @@
 import ReactOnRails from 'react-on-rails'
 
+import NavBar from '../bundles/NavBar/NavBarServer'
 import Directory from '../bundles/DirectoryBundle'
 
 ReactOnRails.register({
+  NavBar,
   Directory
 })
