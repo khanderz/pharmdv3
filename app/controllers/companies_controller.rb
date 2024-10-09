fcb343de6c9e20deb45b37ed1fdd8f2d3d37545d
Khanh <82117702+khanderz@users.noreply.github.com>
2024-10-09 14:29:05 -0500

    add enums

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,13 +4,31 @@ class CompaniesController < ApplicationController
   # GET /companies or /companies.json
   def index
     @companies = Company.includes(:job_posts).all
-    render json: @companies.to_json(include: :job_posts)  
+
+    # Map the enums to human-readable values for each company in the response
+    companies_with_enums = @companies.map do |company|
+      company.as_json(include: :job_posts).merge(
+        company_type: Company::COMPANY_TYPES[company.company_type],
+        pharmacy_type: company.pharmacy_type.present? ? Company::PHARMACY_TYPES[company.pharmacy_type] : nil,
+        digital_health_type: company.digital_health_type.present? ? Company::DIGITAL_HEALTH_TYPES[company.digital_health_type] : nil
+      )
+    end
+
+    render json: companies_with_enums
   end
   
   # GET /companies/1 or /companies/1.json
   def show
     @company = Company.includes(:job_posts).find(params[:id])
-    render json: @company.to_json(include: :job_posts) # Include job_posts in the JSON response
+    
+    # Add human-readable values to the company JSON response
+    company_with_enums = @company.as_json(include: :job_posts).merge(
+      company_type: Company::COMPANY_TYPES[@company.company_type],
+      pharmacy_type: @company.pharmacy_type.present? ? Company::PHARMACY_TYPES[@company.pharmacy_type] : nil,
+      digital_health_type: @company.digital_health_type.present? ? Company::DIGITAL_HEALTH_TYPES[@company.digital_health_type] : nil
+    )
+
+    render json: company_with_enums
   end
 
   # GET /companies/new
@@ -61,13 +79,31 @@ class CompaniesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_company
-      @company = Company.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def company_params
-      params.require(:company).permit(:company_name, :operating_status, :company_type, :company_type_value, :company_ats_type, :company_size, :last_funding_type, :linkedin_url, :is_public, :year_founded, :company_city, :company_state, :company_country, :acquired_by, :ats_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def company_params
+    params.require(:company).permit(
+      :company_name, 
+      :operating_status, 
+      :company_type, 
+      :pharmacy_type, 
+      :digital_health_type, 
+      :company_ats_type, 
+      :company_size, 
+      :last_funding_type, 
+      :linkedin_url, 
+      :is_public, 
+      :year_founded, 
+      :company_city, 
+      :company_state, 
+      :company_country, 
+      :acquired_by, 
+      :ats_id
+    )
+  end
 end
