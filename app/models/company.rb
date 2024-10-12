c134b94f965f842eb2216a1ac06d05dc9cf62977
Khanh <82117702+khanderz@users.noreply.github.com>
2024-10-12 17:20:43 -0500

    update model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,29 @@
+# app/models/company.rb
 class Company < ApplicationRecord
-  belongs_to :company_specialty
+  has_many :job_posts, foreign_key: :companies_id, dependent: :destroy
+  
+	#  has_many :company_specializations 
+	 # has_many :company_specialties, through: :company_specializations
+  
+belongs_to :company_specialty
   belongs_to :company_type
+
+  validate :validate_specialty_against_type
+
+  def validate_specialty_against_type
+    allowed_specialties = case company_type&.key
+                          when 'PHARMA'
+                            Company::PHARMACY_TYPES.keys
+                          when 'DIGITAL_HEALTH'
+                            Company::DIGITAL_HEALTH_TYPES.keys
+                          else
+                            []
+                          end
+
+    company_specialties.each do |specialty|
+      unless allowed_specialties.include?(specialty.key)
+        errors.add(:company_specialty, "#{specialty.key} is not valid for the selected company type")
+      end
+    end
+  end
 end
--- app/models/company_specialty.rb
+++ app/models/company_specialty.rb
@@ -1,3 +1,6 @@
+# app/models/company_specialty.rb
 class CompanySpecialty < ApplicationRecord
   belongs_to :company_type
+  has_many :company_specializations
+  has_many :companies, through: :company_specializations
 end
