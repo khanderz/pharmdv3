6ababa3df0e8a67347a3c3d7ea311e215576e645
Khanh <82117702+khanderz@users.noreply.github.com>
2024-10-18 17:25:32 -0500

    abtract more class methods for job post

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -54,27 +54,38 @@ class JobPost < ApplicationRecord
     end
   end
 
+  # get job url
+  def self.get_job_url(ats_code, job)
+    case ats_code
+    when 'LEVER'
+      job['hostedUrl']
+    when 'GREENHOUSE'
+      job['absolute_url']
+    end
+  end
+
   private
 
 #  fetch jobs from ATS APIs
   def self.fetch_jobs(company)
     jobs = get_jobs(company)
+    ats_code = company.ats_type.ats_type_code
 
-    if company.ats_type.ats_type_code == 'LEVER'
+    if ats_code == 'LEVER'
       if jobs.present?
-        active_job_ids = save_lever_jobs(company, jobs)
+        active_job_ids = save_jobs(ats_code,company, jobs)
         deactivate_old_jobs(company, active_job_ids)
       end
-    elsif company.ats_type.ats_type_code == 'GREENHOUSE'
+    elsif ats_code == 'GREENHOUSE'
       if jobs && jobs["jobs"]
         jobs_mapped = jobs["jobs"].map { |job| job }
-        active_job_ids = save_greenhouse_jobs(company, jobs_mapped)
+        active_job_ids = save_jobs(ats_code, company, jobs_mapped)
         deactivate_old_jobs(company, active_job_ids)
       else
         puts "No jobs found for #{company.company_name}"
       end
     else
-      puts "ATS type #{company.ats_type.ats_type_code} not supported for company: #{company.company_name}"
+      puts "ATS type #{ats_code} not supported for company: #{company.company_name}"
     end
   end
 
@@ -151,71 +162,53 @@ class JobPost < ApplicationRecord
     end
   end
 
-  # Lever jobs -----------------------------------------------------------
-  def self.save_lever_jobs(company, jobs)
+  # get job role params
+  def self.get_job_role_params(ats_code, job)
+    case ats_code
+    when 'LEVER'
+      [job['text'], job['categories']['department'], job['categories']['team']]
+    when 'GREENHOUSE'
+      [job["title"], job["departments"][0]["name"], job['departments'][0]["name"]]
+    else
+      puts "ATS type #{ats_code} not supported"
+    end
+  end
+
+  # safe jobs from ATS APIs
+  def self.save_jobs(ats_code, company, jobs)
     active_job_ids = []
     job_count = 0  
     build_count = 0
 
     jobs.each do |job|
-      job_role = JobRole.find_or_create_with_department_and_team(job['text'], job['categories']['department'], job['categories']['team'])
-      unless job_role.persisted?
-        puts "Job Role creation failed for #{job['text']}"
-        next
-      end
+      job_role_params = get_job_role_params(ats_code, job)
+      role_name, department_name, team_name = job_role_params
+
+      job_role = JobRole.find_or_create_with_department_and_team(role_name, department_name, team_name)
+      job_url = get_job_url(ats_code, job)
 
       # Map data values to job post fields
-      mapped_data = map_ats_data_return('lever', job, company)
+      mapped_data = map_ats_data_return(ats_code, job, company)
       job_count += 1
-      
+
       # Prepare new job post data
       job_post_data = build_job_post_data(company, mapped_data, job_role)
       build_count += 1
-      
+
       # Use the helper method to handle checking/updating existing jobs
-      active_job_ids << process_existing_or_new_job(company, job['hostedUrl'], job_post_data)
+      active_job_ids << process_existing_or_new_job(company, job_url, job_post_data)
     end
 
-    puts "Mapped #{job_count} jobs from Lever"
+    puts "Mapped #{job_count} jobs from #{ats_code}"
     puts "Built #{build_count} job posts"
     active_job_ids
-  end
-
-  # Greenhouse jobs -----------------------------------------------------------
-  def self.save_greenhouse_jobs(company, jobs)
-    active_job_ids = []
-    job_count = 0  
-    build_count = 0
-
-    jobs.each do |job|
-      job_role = JobRole.find_or_create_with_department_and_team(job["title"], job["departments"][0]["name"], job['departments'][0]["name"])
-      unless job_role.persisted?
-        puts "Job Role creation failed for #{job['title']}"
-        next
-      end
-      
-      # Map data values to job post fields
-      mapped_data = map_ats_data_return('greenhouse', job, company)
-      job_count += 1
-      
-      # Prepare the new job post data
-      job_post_data = build_job_post_data(company, mapped_data, job_role)
-      build_count += 1
-      
-      # Use the helper method to handle checking/updating existing jobs
-      active_job_ids << process_existing_or_new_job(company, job["absolute_url"], job_post_data)
-    end
-
-    puts "Mapped #{job_count} jobs from Greenhouse"
-    puts "Built #{build_count} job posts"
-    active_job_ids
-  end
+  end 
 
   # Map data values to job post fields
   def self.map_ats_data_return(ats, job, company)
     puts "json: #{job['categories']['allLocations']} "
 
-    if ats == 'lever'
+    if ats == 'LEVER'
       {
         job_title: job['text'],
         job_country_id: handle_country_record(job['country'], job['country'], company.id, job['hostedUrl']), 
@@ -233,7 +226,7 @@ class JobPost < ApplicationRecord
         job_salary_currency_id: handle_currency_record(job['salaryRange']&.dig('currency'), company.id, job['hostedUrl']), 
         job_salary_interval_id: find_interval_id_by_name(job['salaryRange']&.dig('interval')) 
       }
-    elsif ats == 'greenhouse'
+    elsif ats == 'GREENHOUSE'
       {
         job_title: job["title"],
         # job_country_id: handle_country_record(job['country'], job['country'], company.id, job['absolute_url']), 
