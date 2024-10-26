dc21ed10b80bf5aa313d1dbbafb764883b5bef91
Khanh <82117702+khanderz@users.noreply.github.com>
2024-10-26 14:18:52 -0500

    migrate job role filter to v6

--- app/javascript/src/components/molecules/Filters/JobRoleFilter.tsx
+++ app/javascript/src/components/molecules/Filters/JobRoleFilter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Autocomplete } from '@components/atoms/index';
 import { useFiltersContext } from '@javascript/providers/FiltersProvider';
+import { AutocompleteOption } from '@components/atoms/Autocomplete';
 
 export const JobRoleFilter = () => {
   const {
@@ -10,20 +11,32 @@ export const JobRoleFilter = () => {
     jobRolesLoading,
   } = useFiltersContext();
 
+  const options = uniqueJobRoles.map((jobRole) => ({
+    key: jobRole.id,
+    value: jobRole.role_name,
+  }));
+
+  const selectedOptions = selectedJobRoles.map((selected) => {
+    const jobRole = uniqueJobRoles.find((jr) => jr.id === selected.id);
+
+    return {
+      key: jobRole?.id ?? '',
+      value: jobRole?.role_name ?? '',
+    };
+  });
+
   return (
     <Autocomplete
+      id={'job-role-autocomplete'}
       multiple
       inputLabel="Job Roles"
-      options={uniqueJobRoles.map((jobRole) => ({
-        key: jobRole.id,
-        value: jobRole.role_name,
-      }))}
-      value={(selectedJobRoles ?? []).map((jr) => jr.role_name)}
+      options={options}
+      value={selectedOptions}
       onChange={(e, value) => {
-        const selectedValues = (value as string[]).filter(Boolean);
+        const selectedValues = (value as AutocompleteOption[]).filter(Boolean);
 
         const selected = uniqueJobRoles.filter((jobRole) =>
-          selectedValues.includes(jobRole.role_name)
+          selectedValues.some((el) => el.value === jobRole.role_name)
         );
 
         setSelectedJobRoles(selected);
