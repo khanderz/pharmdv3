b7e62c8ec153dcdc76e2fab873adfdbfcf7d106c
Khanh <82117702+khanderz@users.noreply.github.com>
2024-11-25 20:07:37 -0600

    set up job description extraction

--- app/services/job_post_service.rb
+++ app/services/job_post_service.rb
@@ -7,11 +7,6 @@ require 'base64'
 
 class JobPostService
   def self.extract_and_save_salary(job_post)
-    job_title = job_post.job_title.to_s
-    job_description = job_post.job_description.to_s
-    job_qualifications = job_post.job_qualifications.to_s
-    job_additional = job_post.job_additional.to_s
-
     # -------------- 1. Extract salary from job description --------------
     salary_combined_text = "#{job_description} #{job_additional}"
 
@@ -54,5 +49,17 @@ class JobPostService
     else
       puts "Error in salary extraction script: #{stderr} or status: #{status}"
     end
+  end
+  def self.extract_and_save_job_description(job_post)
+    job_title = job_post.job_title.to_s
+    job_description = job_post.job_description.to_s
+    job_qualifications = job_post.job_qualifications.to_s
+    job_responsibilities = job_post.job_responsibilities.to_s
+    job_setting = job_post.job_setting.to_s
+    job_additional = job_post.job_additional.to_s
+
+    # -------------- 2. Extract job description from job description --------------
+    combined_text = "#{job_title} #{job_description} #{job_qualifications} #{job_responsibilities} #{job_setting} #{job_additional}"
+    puts "combined text for job description extraction: #{combined_text}"
   end
 end
