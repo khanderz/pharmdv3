137d6eea5a1f1916282c2a469e490bab1185a44d
Khanh <82117702+khanderz@users.noreply.github.com>
2024-12-27 14:01:26 -0600

    location class method dynamic

--- app/models/location.rb
+++ app/models/location.rb
@@ -35,27 +35,59 @@ class Location < ApplicationRecord
     hierarchy.join(' > ')
   end
 
+  #   create_table 'locations', force: :cascade do |t|
+  #     t.string 'name'
+  #     t.string 'type'
+  #     t.string 'code'
+  #     t.bigint 'parent_id'
+  #     t.string 'aliases', default: [], array: true
+  #     t.text 'error_details'
+  #     t.bigint 'reference_id'
+  #     t.boolean 'resolved'
+  #     t.datetime 'created_at', null: false
+  #     t.datetime 'updated_at', null: false
+  #   end
+
   class << self
-    def find_or_create_by_name_and_type(name, type, attributes = {})
-      normalized_name = name.strip.downcase
-      location = where('LOWER(name) = ? OR ? = ANY(aliases)', normalized_name,
-                       normalized_name).find_by(type: type)
+    def find_or_create_by_name_and_type(location_param, company, job_post = nil)
+      return nil if location_param.blank?
 
-      location ||= create!(
-        name: attributes[:name] || name,
-        type: type,
-        code: attributes[:code],
-        parent_id: attributes[:parent_id],
-        aliases: attributes[:aliases] || []
-      )
+      param = location_param.downcase
+      types = %w[City State Country]
 
-      location
+      types.each do |location_type|
+        existing_location = find_by_name_and_type(param, location_type)
+        return existing_location if existing_location
+
+        error_message = "#{location_type} '#{location_param}' not found for job #{job_post} or company: #{company}"
+        adj_type = job_post ? 'Location' : 'Company'
+
+        puts "#{RED}#{error_message}.#{RESET}"
+
+        new_location = Location.create!(
+          name: location_param,
+          type: location_type,
+          code: nil,
+          parent_id: nil,
+          aliases: [],
+          error_details: error_message,
+          resolved: false
+        )
+
+        Adjudication.log_error(
+          adjudicatable_type: adj_type,
+          adjudicatable_id: new_location.id,
+          error_details: error_message
+        )
+      end
+
+      nil
     end
 
     def find_by_name_and_type(name, type)
       normalized_name = name.strip.downcase
-      where('LOWER(name) = ? OR ? = ANY(aliases)', normalized_name,
-            normalized_name).find_by(type: type)
+      where('(LOWER(name) = ? OR ? = ANY(aliases) OR LOWER(code) = ?)',
+            normalized_name, normalized_name, normalized_name).find_by(type: type)
     end
   end
 end
