a54e3e3aa70ec9914dc10b7c9772b38691d2f29f
Khanh <82117702+khanderz@users.noreply.github.com>
2025-01-01 13:48:20 -0600

    db

---
+++ spec/utils/title_cleaner_spec.rb
@@ -0,0 +1,18 @@
+# spec/lib/utils/title_cleaner_spec.rb
+
+require 'rails_helper'
+require 'utils/title_cleaner'
+
+RSpec.describe Utils::TitleCleaner do
+  describe '.clean' do
+    it 'removes special characters from the title' do
+      title = "Job@Title!!"
+      cleaned_title = described_class.clean(title)
+      expect(cleaned_title).to eq("JobTitle")
+    end
+
+    it 'returns an empty string for nil input' do
+      expect(described_class.clean(nil)).to eq("")
+    end
+  end
+end
