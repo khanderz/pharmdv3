3a600627637b340a26bdd6aa961c2a941e58f5a1
Khanh <82117702+khanderz@users.noreply.github.com>
2024-12-31 19:27:31 -0600

    extra conditions for matching dept and job role

--- app/models/department.rb
+++ app/models/department.rb
@@ -30,24 +30,75 @@ class Department < ApplicationRecord
     cleaned_name = clean_department_name(department_name)
     normalized_name = cleaned_name.downcase
 
+    puts "Finding department: #{cleaned_name}"
+    puts "Normalized name: #{normalized_name}"
+
     if normalized_name.include?('headquarters')
       department = find_by(dept_name: 'Executive')
-      puts "#{GREEN}Mapped '#{department_name}' to 'Executive' department.#{RESET}"
-      return department
+      puts "#{ORANGE}normalized name match to headquarters, matching department to Executive#{RESET}"
+
+      return department if department
+    end
+
+    if normalized_name.include?('clinical')
+      department = find_by(dept_name: 'Clinical Team')
+      puts "#{ORANGE}normalized name match to clinical, matching department to Clinical Team#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('sales')
+      department = find_by(dept_name: 'Sales')
+      puts "#{ORANGE}normalized name match to sales, matching department to Sales#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('marketing')
+      department = find_by(dept_name: 'Marketing')
+      puts "#{ORANGE}normalized name match to marketing, matching department to Marketing#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('operations')
+      department = find_by(dept_name: 'Operations')
+      puts "#{ORANGE}normalized name match to operations, matching department to Operations#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('data')
+      department = find_by(dept_name: 'Data Science')
+      puts "#{ORANGE}normalized name match to data, matching department to Data Science#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('technology')
+      department = find_by(dept_name: 'IT')
+      puts "#{ORANGE}normalized name match to technology, matching department to IT#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('quality')
+      department = find_by(dept_name: 'Quality')
+      puts "#{ORANGE}normalized name match to quality, matching department to Quality#{RESET}"
+      return department if department
+    end
+
+    if normalized_name.include?('science')
+      department = find_by(dept_name: 'Science')
+      puts "#{ORANGE}normalized name match to science, matching department to Science#{RESET}"
+      return department if department
     end
 
     department = where('LOWER(dept_name) = ?', normalized_name)
                  .or(where('aliases::text ILIKE ?', "%#{normalized_name}%"))
                  .first
 
-    if department
-      puts "#{GREEN}Department #{department_name} found in existing records.#{RESET}"
-    else
-      department = Department.create!(
-        dept_name: cleaned_name,
-        error_details: "Department #{department_name} for #{job_url} not found in existing records",
-        resolved: false
-      )
+    puts "Department found: #{department&.dept_name}"
+    department ||= Department.create!(
+      dept_name: cleaned_name.titleize,
+      error_details: "Department #{department_name} for #{job_url} not found in existing records",
+      resolved: false
+    )
+    unless department.resolved
       Adjudication.log_error(
         adjudicatable_type: 'Department',
         adjudicatable_id: department.id,
--- app/models/job_role.rb
+++ app/models/job_role.rb
@@ -19,6 +19,42 @@ class JobRole < ApplicationRecord
     cleaned_title = titles[:cleaned_title].presence || titles[:modified_title]
     modified_title = titles[:modified_title]
 
+    if cleaned_title.include?('account') || modified_title.include?('account')
+      job_role = JobRole.find_by(role_name: 'Account Manager')
+      puts "#{ORANGE}normalized name match to account, matching job role to Account Manager#{RESET}"
+      return job_role if job_role
+    end
+
+    if cleaned_title.include?('legal') || modified_title.include?('legal')
+      job_role = JobRole.find_by(role_name: 'Counsel')
+      puts "#{ORANGE}normalized name match to legal, matching job role to Counsel#{RESET}"
+      return job_role if job_role
+    end
+
+    if cleaned_title.include?('marketing') || modified_title.include?('marketing')
+      job_role = JobRole.find_by(role_name: 'Marketing Specialist')
+      puts "#{ORANGE}normalized name match to marketing, matching job role to Marketing Specialist#{RESET}"
+      return job_role if job_role
+    end
+
+    if cleaned_title.include?('sales') || modified_title.include?('sales')
+      job_role = JobRole.find_by(role_name: 'Sales Representative')
+      puts "#{ORANGE}normalized name match to sales, matching job role to Sales Representative#{RESET}"
+      return job_role if job_role
+    end
+
+    if cleaned_title.include?('laboratory') || modified_title.include?('laboratory')
+      job_role = JobRole.find_by(role_name: 'Laboratory Technician')
+      puts "#{ORANGE}normalized name match to laboratory, matching job role to Laboratory Technician#{RESET}"
+      return job_role if job_role
+    end
+
+    # if cleaned_title.include?('scientist') || modified_title.include?('scientist')
+    #   job_role = JobRole.find_by(role_name: 'Scientist')
+    #   puts "#{ORANGE}normalized name match to scientist, matching job role to Scientist#{RESET}"
+    #   return job_role if job_role
+    # end
+
     job_role = JobRole.find_by('LOWER(role_name) = ?', modified_title.downcase)
     job_role ||= JobRole.find_by('LOWER(role_name) = ?', cleaned_title.downcase)
 
